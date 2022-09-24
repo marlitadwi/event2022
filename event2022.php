67f1488d0f1294a054384cf946c2a94a0abb3999
marlitadwi <113868283+marlitadwi@users.noreply.github.com>
2022-09-24 08:02:13 +0700

    Create event2022.php

---
+++ event2022.php
@@ -0,0 +1,7 @@
+briefing panitia pukul 05.30 wib
+jalan sehat dimulai pukul 06.00 wib
+sarapan bubur bersama setelah jalan sehat pukul 08.00 wib
+senam massal pukul 09.30 wib
+pembagian dorprize pukul 10.00 wib
+lomba anak-anak dan ibu-ibu pukul 13.00 wib
+pembagian hadiah dan penutupan pukul 17.00 wib
