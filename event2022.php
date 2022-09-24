934ff6c3b26f45b9f1c094210a681a6641fb8de9
marlitadwi <113868283+marlitadwi@users.noreply.github.com>
2022-09-24 08:25:11 +0700

    Update event2022.php
    mengubah jam lomba dari jam 13.00 menjadi 11.00 wib

--- event2022.php
+++ event2022.php
@@ -3,5 +3,5 @@ jalan sehat dimulai pukul 06.00 wib
 sarapan bubur bersama setelah jalan sehat pukul 08.00 wib
 senam massal pukul 09.30 wib
 pembagian dorprize pukul 10.00 wib
-lomba anak-anak dan ibu-ibu pukul 13.00 wib
+lomba anak-anak dan ibu-ibu pukul 11.00 wib
 pembagian hadiah dan penutupan pukul 17.00 wib
